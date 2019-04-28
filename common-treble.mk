9042f983568a07c568ee00f980bc9352ff4d4dd7
sjllls <2012ji123@gmail.com>
2019-04-28 18:47:14 +0800

    Remove Sony PowerHAL

--- common-treble.mk
+++ common-treble.mk
@@ -128,10 +128,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
-# Power
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.sony
-
 ifeq ($(AB_OTA_UPDATER),true)
 # Boot control
 PRODUCT_PACKAGES += \
