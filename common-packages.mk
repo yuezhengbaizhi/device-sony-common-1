b75c1b1363dfce687e41991fbcbcd2c5a40ce59e
sjllls <2012ji123@gmail.com>
2019-08-19 11:16:23 +0800

    Common: Add lineage trust Hal

--- common-packages.mk
+++ common-packages.mk
@@ -154,3 +154,7 @@ PRODUCT_PACKAGES += \
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
+
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
