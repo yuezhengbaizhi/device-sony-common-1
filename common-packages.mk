e77445bd21fb629c95d6f23e3ac3b824fcb74ce3
sjllls <2012ji123@gmail.com>
2019-08-19 20:55:12 +0800

    common: remove live display
    This will cause black screen on dora

--- common-packages.mk
+++ common-packages.mk
@@ -147,10 +147,6 @@ PRODUCT_PACKAGES += \
     fs_config_files \
     fs_config_dirs
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
